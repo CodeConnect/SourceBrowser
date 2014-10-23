17426af92b335971a1e22fccefbda1ac68fb4079
AmadeusW <git@amadeusw.com>
2014-10-23 02:59:27 -0400

    #16 makes project a class library, creates console app project
    Changes SourceBrowser.Generator to be a class library (previously,
    console application)
    Removes its Program.cs
    Creates a console application project:
    SourceBrowser.Generator.TestConsole
    Implements a simple test app.

---
+++ src/SourceBrowser.Generator.TestConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using SourceBrowser.Generator;
+
+namespace SourceBrowser.Generator.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Combined with, or replaced by, provided paths to create absolute paths
+            string userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            // For developers that set test variables in code:
+            string solutionPath = @"Documents\GitHub\Kiwi\TestSolution\TestSolution.sln";
+            string saveDirectory = @"Documents\SourceBrowserResult\";
+
+            // For developers that provide test variables in arguments:
+            if (args.Length == 2)
+            {
+                solutionPath = args[0];
+                saveDirectory = args[1];
+            }
+
+            // Combine user's root with relative solution paths, or use absolute paths.
+            // (Path.Combine returns just second argument if it is an absolute path)
+            string absoluteSolutionPath = Path.Combine(userProfilePath, solutionPath);
+            string absoluteSaveDirectory = Path.Combine(userProfilePath, saveDirectory);
+
+            try
+            {
+                Console.Write("Opening " + absoluteSolutionPath);
+                Console.WriteLine("...");
+
+                var solutionAnalyzer = new SolutionAnalayzer(absoluteSolutionPath);
+
+                Console.Write("Analyzing and saving into " + absoluteSaveDirectory);
+                Console.WriteLine("...");
+
+                solutionAnalyzer.AnalyzeAndSave(saveDirectory);
+
+                Console.WriteLine("Job successful!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
