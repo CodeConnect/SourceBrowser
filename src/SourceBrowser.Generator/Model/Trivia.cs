267d897d3f887c3c2d7482eda0cf1cb7bbc9abf3
Josh Varty <joshvarty@gmail.com>
2014-11-02 15:44:57 -0500

    Added support for Trivia.
    We need to retain info on trivia in order to color it correctly.
    Eventually, we made want to support linked trivia. However, to my
    knowledge this is only present in C# and VB .Net, so it might not be
    worth worrying about just yet.

---
+++ src/SourceBrowser.Generator/Model/Trivia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceBrowser.Generator.Model
+{
+    public class Trivia
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
