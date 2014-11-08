8fca6e40be9449c1f9e0587d5a05fe3d7283c470
Amadeus Wieczorek <git@amadeusw.com>
2014-11-08 16:27:38 -0500

    Creates SourceBrowser.Site project

---
+++ src/SourceBrowser.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SourceBrowser.Site.Startup))]
+namespace SourceBrowser.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
