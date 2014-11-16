d14ed50b510cf086bd6496fb5b4e4cfcc5b2cf3d
Amadeus Wieczorek <git@amadeusw.com>
2014-11-16 18:36:38 -0500

    Uses Octokit to retrieve user and repo data
    Removes reference to newtonsoft.json as we're not parsing json data.

--- src/SourceBrowser.SolutionRetriever/GitHubInformationRetriever.cs
+++ src/SourceBrowser.SolutionRetriever/GitHubInformationRetriever.cs
@@ -5,88 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
-using Newtonsoft.Json.Linq;
+using Octokit;
 
 namespace SourceBrowser.SolutionRetriever
 {
-    public class GitHubInformationRetriever : IInformationRetriever
+    public class GitHubInformationRetriever
     {
-        // TODO: Figure out the best location for the GitHubInformationRetriever class
-        // This also exists in SourceBrowser.Site.Repositories.BrowseRepository
-        private static readonly string ProjectAbsolutePath = System.Web.Hosting.HostingEnvironment.MapPath("~/SB_Files/");
+        private static GitHubClient _github = new GitHubClient(new ProductHeaderValue("SourceBrowser"));
 
-        #region IInformationRetriever implementation
-
-        public bool TryUpdateUserInformation(string userName)
+        public static void GetUserInformation(string username, ref string fullName, ref string avatarUrl, ref string gitHubUrl, ref string blogUrl)
         {
-            string userInformationPath = Path.Combine(ProjectAbsolutePath, userName, "user.json");
-            if (fileIsFresh(userInformationPath))
-            {
-                return false;
-            }
-            updateUserInformation(userName, userInformationPath);
-            return true;
+            var user = _github.User.Get(username).Result;
+
+            avatarUrl = user.AvatarUrl;
+            gitHubUrl = user.HtmlUrl;
+            blogUrl = user.Blog;
+            fullName = user.Name;
         }
 
-        public bool TryUpdateRepoInformation(string userName, string repoName)
+        public static void GetRepoInformation(string userName, string repoName, ref int forksCount, ref int starsCount, ref string language, ref string homepage, ref bool isPrivate)
         {
-            string repoInformationPath = Path.Combine(ProjectAbsolutePath, userName, repoName, "repo.json");
-            if (fileIsFresh(repoInformationPath))
-            {
-                return false;
-            }
-            updateRepoInformation(userName, repoName, repoInformationPath);
-            return true;
+            var repo = _github.Repository.Get(userName, repoName).Result;
+
+            forksCount = repo.ForksCount;
+            starsCount = repo.StargazersCount;
+            language = repo.Language;
+            homepage = repo.Homepage;
+            isPrivate = repo.Private;
         }
 
-        public JObject GetUserInformation(string userName)
-        {
-            string userInformationPath = Path.Combine(ProjectAbsolutePath, userName, "user.json");
-            return JObject.Parse(File.ReadAllText(userInformationPath));
-        }
-
-        public JObject GetRepoInformation(string userName, string repoName)
-        {
-            string repoInformationPath = Path.Combine(ProjectAbsolutePath, userName, repoName, "repo.json");
-            return JObject.Parse(File.ReadAllText(repoInformationPath));
-        }
-
-        #endregion
-
-        private void updateUserInformation(string userName, string userInformationPath)
-        {
-            Uri requestURL = new Uri("https://api.github.com/users/" + userName);
-            using (var webClient = new WebClient())
-            {
-                webClient.DownloadFileAsync(requestURL, userInformationPath);
-            }
-        }
-
-        private void updateRepoInformation(string userName, string repoName, string repoInformationPath)
-        {
-            Uri requestURL = new Uri("https://api.github.com/repos/" + userName + "/" + repoName);
-            using (var webClient = new WebClient())
-            {
-                webClient.DownloadFileAsync(requestURL, repoInformationPath);
-            }
-        }
-
-        private bool fileIsFresh(string filePath)
-        {
-            // If file doesn't exist, it's not "fresh" (it needs to be downloaded)
-            if (!File.Exists(filePath))
-            {
-                return false;
-            }
-
-            // If file has been modified within last 60 minutes, it is considered "fresh"
-            DateTime freshThreshold = DateTime.Now - TimeSpan.FromMinutes(60);
-            if (File.GetLastWriteTimeUtc(filePath) > freshThreshold.ToUniversalTime())
-            {
-                return true;
-            }
-
-            return false;
-        }
     }
 }
