0473c93f14fe898f6035230303436a5e7f880a6d
Josh Varty <joshvarty@gmail.com>
2014-11-20 20:35:57 -0500

    Initial structure of HtmlTransformer

--- src/SourceBrowser.Generator/Transformers/HtmlTransformer.cs
+++ src/SourceBrowser.Generator/Transformers/HtmlTransformer.cs
@@ -1,6 +1,7 @@
 ﻿using SourceBrowser.Generator.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,10 +28,80 @@ namespace SourceBrowser.Generator.Transformers
 
         protected override void VisitDocument(DocumentModel documentModel)
         {
-            var path = documentModel.ContainingPath;
+            var documentSavePath = Path.Combine(_savePath, documentModel.ContainingPath, documentModel.Name);
             //TODO: Write the HTML to the appropriate path
 
+            StringBuilder sb = new StringBuilder(documentModel.Tokens.Count);
+            var tokenTypes = documentModel.Tokens.Select(n => n.Type).Distinct();
+            var leadingTriviaTypes = documentModel.Tokens.SelectMany(n => n.LeadingTrivia.Select(m => m.Type)).Distinct();
+            var trailingTriviaTypes = documentModel.Tokens.SelectMany(n => n.TrailingTrivia.Select(m => m.Type)).Distinct();
+
+            foreach (var token in documentModel.Tokens)
+            {
+                processToken(sb, token);
+            }
+
             base.VisitDocument(documentModel);
+        }
+
+        private void processToken(StringBuilder sb, Token token)
+        {
+            processTriviaCollection(sb, token.LeadingTrivia);
+
+            switch (token.Type)
+            {
+                case "Keyword":
+                    processKeyword(sb, token);
+                    break;
+                case "IdentifierToken":
+                    processIdentifier(sb, token);
+                    break;
+                case "Other":
+                    processOther(sb, token);
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid token type");
+            }
+
+            processTriviaCollection(sb, token.TrailingTrivia);
+        }
+
+        private void processIdentifier(StringBuilder sb, Token token)
+        {
+            sb.Append(token.Value);
+        }
+
+        private void processOther(StringBuilder sb, Token token)
+        {
+            sb.Append(token.Value);
+        }
+
+        private void processKeyword(StringBuilder sb, Token token)
+        {
+            sb.Append(token.Value);
+        }
+
+
+        private void processTriviaCollection(StringBuilder sb, ICollection<Trivia> triviaCollection)
+        {
+            foreach(var trivia in triviaCollection)
+            {
+                processTrivia(sb, trivia);
+            }
+        }
+
+        private void processTrivia(StringBuilder sb, Trivia trivia)
+        {
+            if(trivia.Type.Contains("Comment"))
+            {
+                sb.Append("<span class='comment'>");
+                sb.Append(trivia.Value);
+                sb.Append("</span>");
+            }
+            else
+            {
+                sb.Append(trivia.Value);
+            }
         }
     }
 }
