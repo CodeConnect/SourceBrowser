45c5255cab9741f45de97918d7356d231c027280
Amadeus Wieczorek <git@amadeusw.com>
2014-11-25 23:07:24 -0500

    Initial implementation of TreeViewTransformer

--- src/SourceBrowser.Generator/Transformers/TreeViewTransformer.cs
+++ src/SourceBrowser.Generator/Transformers/TreeViewTransformer.cs
@@ -1,9 +1,7 @@
-﻿using SourceBrowser.Generator.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Web.UI;
+using SourceBrowser.Generator.Model;
 
 namespace SourceBrowser.Generator.Transformers
 {
@@ -14,30 +12,55 @@ namespace SourceBrowser.Generator.Transformers
     public class TreeViewTransformer : AbstractWorkspaceVisitor
     {
         private string _savePath;
+        StringWriter _stringWriter;
+        HtmlTextWriter _writer;
 
         public TreeViewTransformer(string savePath)
         {
             _savePath = savePath;
+            _stringWriter = new StringWriter();
+            _writer = new HtmlTextWriter(_stringWriter);
         }
 
         protected override void VisitWorkspace(WorkspaceModel workspaceModel)
         {
             base.VisitWorkspace(workspaceModel);
+            _writer.RenderEndTag();
         }
 
         protected override void VisitProjectItem(IProjectItem projectItem)
         {
+            _writer.AddAttribute(HtmlTextWriterAttribute.Id, getFullPath(projectItem));
+            _writer.RenderBeginTag(HtmlTextWriterTag.Ul);
             base.VisitProjectItem(projectItem);
+            _writer.RenderEndTag();
         }
 
         protected override void VisitFolder(FolderModel folderModel)
         {
+            _writer.AddAttribute(HtmlTextWriterAttribute.Id, getFullPath(folderModel));
+            _writer.RenderBeginTag(HtmlTextWriterTag.Ul);
             base.VisitFolder(folderModel);
+            _writer.RenderEndTag();
         }
 
         protected override void VisitDocument(DocumentModel documentModel)
         {
+            _writer.RenderBeginTag(HtmlTextWriterTag.Li);
             base.VisitDocument(documentModel);
+            _writer.RenderEndTag();
+        }
+
+        private string getFullPath(IProjectItem item)
+        {
+            if (item.Parent != null)
+            {
+                return getFullPath(item.Parent) + "/" + item.Name;
+            }
+            else
+            {
+                return item.Name;
+            }
         }
     }
 }
