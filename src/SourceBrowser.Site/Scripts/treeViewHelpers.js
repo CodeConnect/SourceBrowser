7060a0e5e1d58bc6c846353de81febdf47b65910
Amadeus Wieczorek <git@amadeusw.com>
2014-12-01 15:05:39 -0500

    Fixes the Tree View js
    The ID selector had an asterisk which caused it to match more than what
    we wanted.
    Also now I'm showing the branches of the tree that need to be expanded.

--- src/SourceBrowser.Site/Scripts/treeViewHelpers.js
+++ src/SourceBrowser.Site/Scripts/treeViewHelpers.js
@@ -33,11 +33,16 @@ function expandTreeView(path) {
 
 function expandNode(path) {
     //console.log(path);
-    var selector = "li[id*='" + path + "']";
+    var selector = "li[id='" + path + "']";
     //console.log("Looking for " + selector);
     var node = $(selector);
+    var child = $(selector + " > ul");
     if (node.length) {
-        console.log("Found " + path);
+        console.log("Found node " + path);
         node.removeClass("collapsed");
+    }
+    if (child.length) {
+        console.log("Found child " + path);
+        child.show();
     }
 }
