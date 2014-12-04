103208a46eba4793c5c2cc63d4dbc6670aaef62d
Josh Varty <joshvarty@gmail.com>
2014-12-04 13:54:17 -0500

    Initial commit of SearchIndexTransformer

---
+++ src/SourceBrowser.Generator/Transformers/SearchIndexTransformer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceBrowser.Generator.Model;
+using System.IO;
+
+namespace SourceBrowser.Generator.Transformers
+{
+    public class SearchIndexTransformer : AbstractWorkspaceVisitor
+    {
+        private string _username;
+        private string _repository;
+
+        public SearchIndexTransformer(string username, string repository)
+        {
+            _username = username;
+            _repository = repository;
+        }
+        protected override void VisitDocument(DocumentModel documentModel)
+        {
+            var documentId = Path.Combine(_username, _repository, documentModel.RelativePath);
+            var declarations = documentModel.Tokens.Where(n => n.IsDeclaration);
+
+            foreach(var declaration in declarations)
+            {
+                //Add to index
+            }
+
+            base.VisitDocument(documentModel);
+        }
+    }
+}
