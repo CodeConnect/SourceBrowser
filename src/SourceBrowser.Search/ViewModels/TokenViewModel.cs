3562ae206c9262f7a6c8a118ed8d091e15d3c2e0
Josh Varty <joshvarty@gmail.com>
2014-12-04 21:07:14 -0500

    Add container struct for search index values.

---
+++ src/SourceBrowser.Search/ViewModels/TokenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceBrowser.Search.ViewModels
+{
+    public struct TokenViewModel
+    {
+        public string DocumentId { get; }
+        public string FullName { get; }
+        public int LineNumber { get; }
+
+        public TokenViewModel(string documentId, string fullName, int lineNumber)
+        {
+            DocumentId = documentId;
+            FullName = fullName;
+            LineNumber = lineNumber;
+        }
+    }
+}
