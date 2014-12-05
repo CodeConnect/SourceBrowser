5d9691f05786aba7938c82db7babccd8cb73d469
Josh Varty <joshvarty@gmail.com>
2014-12-04 22:59:16 -0500

    Initial structure for searching the index.

--- src/SourceBrowser.Search/SearchIndex.cs
+++ src/SourceBrowser.Search/SearchIndex.cs
@@ -1,6 +1,7 @@
 ﻿using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
 using SourceBrowser.Search.ViewModels;
@@ -57,9 +58,58 @@ namespace SourceBrowser.Search
 
             doc.Add(new Field("Id", documentId, Field.Store.YES, Field.Index.NOT_ANALYZED));
             doc.Add(new Field("Name", fullName, Field.Store.YES, Field.Index.ANALYZED));
-            doc.Add(new Field("Line", lineNumber.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            doc.Add(new Field("Lines", lineNumber.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
 
             writer.AddDocument(doc);
+        }
+
+        private static Query parseQuery(string searchQuery, QueryParser parser)
+        {
+            Query query;
+            try
+            {
+                query = parser.Parse(searchQuery.Trim());
+            }
+            catch(ParseException)
+            {
+                query = parser.Parse(QueryParser.Escape(searchQuery.Trim()));
+            }
+            return query;
+        }
+
+        private static TokenViewModel mapDocumentToToken(Document document)
+        {
+            return new TokenViewModel(
+                document.Get("Id"),
+                document.Get("Name"),
+                Convert.ToInt32(document.Get("Lines"))
+                );
+        }
+
+        private static IEnumerable<TokenViewModel> MapLuceneToDataList(IEnumerable<ScoreDoc> hits, IndexSearcher searcher)
+        {
+            return hits.Select(hit => mapDocumentToToken(searcher.Doc(hit.Doc))).ToList();
+        }
+
+
+        private static IEnumerable<TokenViewModel> search(string searchQuery, string searchField = "")
+        {
+            if (string.IsNullOrEmpty(searchQuery.Replace("*", "").Replace("?", "")))
+                return new List<TokenViewModel>();
+
+            using (var searcher = new IndexSearcher(_directory, false))
+            using (var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30))
+            {
+                var hitsLimit = 100;
+
+                var parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, "FullName", analyzer);
+                var query = parseQuery(searchQuery, parser);
+                var hits = searcher.Search(query, hitsLimit).ScoreDocs;
+
+                var results = MapLuceneToDataList(hits, searcher);
+                analyzer.Close();
+                return results;
+            }
         }
     }
 }
