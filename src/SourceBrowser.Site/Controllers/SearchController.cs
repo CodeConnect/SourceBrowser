5cba90604d90ca5020b176322ac86b65dd35be2f
Josh Varty <joshvarty@gmail.com>
2014-12-05 00:46:54 -0500

    First commit of search controller.

---
+++ src/SourceBrowser.Site/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceBrowser.Site.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Repository(string username, string repo, string query)
+        {
+            var x = new JsonResult();
+            x.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return x;
+        }
+    }
+}
