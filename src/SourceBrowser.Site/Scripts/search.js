344128a44fdefbb4d4233b3b34004c880df799e8
Josh Varty <joshvarty@gmail.com>
2014-12-10 01:54:22 -0500

    Refactored search javascript.

--- src/SourceBrowser.Site/Scripts/search.js
+++ src/SourceBrowser.Site/Scripts/search.js
@@ -1,93 +1,118 @@
-﻿$("#search-form").submit(function (event) {
-    var query = $("#search-box").val();
-    var path = window.location.pathname;
-    //Split path, removing empty entries
-    var splitPath = path.split('/');
-    splitPath = splitPath.filter(function (v) { return v !== '' });
+﻿search = {
+    //Keeps track of whether a search is ongoing.
+    //We only want to send one query at a time.
+    isSearching: false,
+    //Retains the state of the most recent query. 
+    //When we're done searching, we check the text box to see 
+    //if the query has changed.
+   lastQuery: "",
+   beginSearch: function () {
+        if (search.isSearching)
+            return;
+        var query = $("#search-box").val();
+        search.lastQuery = query;
+        //Split path, removing empty entries
+        var path = window.location.pathname;
+        var splitPath = path.split('/');
+        splitPath = splitPath.filter(function (v) { return v !== '' });
 
-    if (splitPath.length <= 1) {
-        searchSite(query);
-    }
-    else if (splitPath.length >= 2)
-    {
-        var username = splitPath[1];
-        var repository = splitPath[2];
-        searchRepository(username, repository, query);
-    }
+        //Only search if we're in a repository
+        if (splitPath.length >= 2) {
+            var username = splitPath[1];
+            var repository = splitPath[2];
 
-    return false;
+            search.searchRepository(username, repository, query);
+        }
+
+        return false;
+    },
+
+    
+   searchRepository: function (username, repository, query) {
+        data = JSON.stringify({
+            "username": username,
+            "repository": repository,
+            "query": query
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "/Search/Repository/",
+            //TODO: If anyone knows a better way to do this, please share it.
+            //My Javascript is not strong...
+            data: "username=" + username + "&repository=" + repository + "&query=" + query,
+            success: search.handleSuccess,
+            error: search.handleError
+        });
+    },
+
+    handleError: function (e) {
+        console.log(e);
+        search.isSearching = false;
+        search.checkIfSearchTextChanged();
+    },
+
+    handleSuccess: function (results) {
+        console.log(results);
+        $("#tree-view").hide();
+        //If we can't find any results, tell the user.
+        if (results.length == 0) {
+            $("#search-results").hide();
+            $("#no-results").show();
+        }
+        else {
+            $("#no-results").hide();
+            $("#search-results").show();
+            var htmlResults = search.buildResults(results);
+            $("#search-results").empty().append(htmlResults);
+        }
+        search.isSearching = false;
+        search.checkIfSearchTextChanged();
+    },
+
+    buildResults: function(results) {
+        var htmlResults = "";
+        for (var i = 0; i < results.length; i++) {
+            var searchResult = results[i];
+            var lineNumber = searchResult["LineNumber"];
+            var link = "/Browse/" + searchResult["Path"] + "#" + lineNumber;
+            html = "";
+            html += "<a href='" + link + "'>";
+            html += "<span>"
+            html += searchResult["Name"];
+            html += "</span>";
+            html += "<span>"
+            html += searchResult["FullName"];
+            html += "</span>";
+            html += "</a>";
+
+            htmlResults += html;
+        }
+        return htmlResults;
+    },
+
+    //After searching, we need to check to see if the user has typed
+    //any additional characters into search while we were waiting
+    //for the server
+    checkIfSearchTextChanged : function() {
+        var currentQuery = $("#search-box").val();
+        if (currentQuery != search.lastQuery)
+            search.beginSearch();
+    },
+}
+
+
+$("#search-box").keyup(function () {
+    search.beginSearch();
 });
 
 
-function searchRepository(username, repository, query) {
-
-    data = JSON.stringify({
-        "username": username,
-        "repository": repository,
-        "query": query
-    });
-
-    $.ajax({
-        type: "POST",
-        url: "/Search/Repository/",
-        //TODO: If anyone knows a better way to do this, please share it.
-        //My Javascript is not strong...
-        data: "username=" + username + "&repository=" + repository + "&query="+ query,
-        success: handleSearch
-    });
-}
-
-function handleSearch(results) {
-    $("#tree-view").hide();
-
-    if (results.length == 0) {
-        $("#search-results").hide();
-        $("#no-results").show();
-    }
-    else {
-        $("#no-results").hide();
-        $("#search-results").show();
-        var htmlResults = buildResults(results);
-        $("#search-results").empty().append(htmlResults);
-    }
-}
-
-$("#search-box").keyup(function (e) {
-    var val = $("#search-box").val();
-    if (val.length == 0) {
-        $("#search-results").hide();
-        $("#no-results").hide();
-        $("#tree-view").show();
-    }
-    else {
-        console.log(val);
-        $("#search-form").submit();
-    }
-});
-
-function buildResults(results) {
-    var htmlResults = "";
-    for (var i = 0; i < results.length; i++) {
-        var searchResult = results[i];
-        console.log(searchResult);
-        var lineNumber = searchResult["LineNumber"];
-        var link = "/Browse/" + searchResult["Path"] + "#" + lineNumber;
-        html = "";
-        html += "<a href='" + link + "'>";
-        html += "<span>"
-        html += searchResult["Name"];
-        html += "</span>";
-        html += "<span>"
-        html += searchResult["FullName"];
-        html += "</span>";
-        html += "</a>";
-
-        htmlResults += html;
-    }
-    return htmlResults;
-}
 
 
-function searchSite(query) {
-}
+
+
+
+
+
+
 
