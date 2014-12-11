88cb8116cfc126595ecb93e8e772523304374b0d
Josh Varty <joshvarty@gmail.com>
2014-12-11 18:53:53 -0500

    Add attribute to detect AJAX requests.

---
+++ src/SourceBrowser.Site/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceBrowser.Site.Attributes
+{
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.RequestContext.HttpContext.Request.IsAjaxRequest();
+        }
+    }
+}
