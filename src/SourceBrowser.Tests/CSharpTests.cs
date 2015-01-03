7a56fac3a17544b7acdd2b2481ad9bd164f930a2
Josh Varty <joshvarty@gmail.com>
2015-01-02 19:03:06 -0700

    Add basic linking tests. Remove PublicKey
    We don't need to be signing these things as we're not working with
    anyone else's internals.

--- src/SourceBrowser.Tests/CSharpTests.cs
+++ src/SourceBrowser.Tests/CSharpTests.cs
@@ -20,11 +20,12 @@ namespace SourceBrowser.Tests
                 Project(
                     ProjectName("Project1"),
                     Sign,
-                    Document(string.Format(
+                    Document(
                         @"
                         class C1
                         {{
-                        }}", PublicKey))));
+                        }}")));
+
             WorkspaceModel ws = new WorkspaceModel("Workspace1", "");
             FolderModel fm = new FolderModel(ws, "Project1");
 
@@ -35,8 +36,52 @@ namespace SourceBrowser.Tests
             walker.Visit(document.GetSyntaxRootAsync().Result);
             var documentModel = walker.GetDocumentModel();
 
+            //Make sure there's five tokens
             Assert.IsTrue(documentModel.Tokens.Count == 5);
+        }
 
+        [TestMethod]
+        public void BasicLinking()
+        {
+            var solution = base.Solution(
+             Project(
+                 ProjectName("Project1"),
+                 Sign,
+                 Document(
+                    @"
+                    class C1
+                    {
+                        public void Method1()
+                        {
+                            Method2();
+                        }
+                        public void Method2()
+                        {
+                            Method1();
+                        }
+                    }")));
+
+            WorkspaceModel ws = new WorkspaceModel("Workspace1", "");
+            FolderModel fm = new FolderModel(ws, "Project1");
+
+            var document = solution.Projects.SelectMany(n => n.Documents).Where(n => n.Name == "Document1.cs").Single();
+            var linkProvider = new ReferencesourceLinkProvider();
+
+            var walker = new SourceBrowser.Generator.DocumentWalkers.CSWalker(fm, document, linkProvider);
+            walker.Visit(document.GetSyntaxRootAsync().Result);
+            var documentModel = walker.GetDocumentModel();
+
+            //Make sure there's two links
+            var links = documentModel.Tokens.Select(n => n.Link).Where(n => n != null);
+            Assert.IsTrue(links.Count() == 2);
+
+            //Make sure they're both symbol links
+            Assert.IsTrue(links.First() is SymbolLink);
+            Assert.IsTrue(links.Last() is SymbolLink);
+
+            //Make sure they link correctly
+            Assert.IsTrue(((SymbolLink)links.First()).ReferencedSymbolName == "C1.Method2()");
+            Assert.IsTrue(((SymbolLink)links.Last()).ReferencedSymbolName == "C1.Method1()");
         }
     }
 }
