0fafe28eb839e9bf11b79860f8ab82cec51abf04
Amadeus Wieczorek <git@amadeusw.com>
2015-01-16 18:08:23 -0800

    Fixes 2 commits ago tabs were used instead of spaces

--- src/SourceBrowser.Shared/Constants.cs
+++ src/SourceBrowser.Shared/Constants.cs
@@ -8,8 +8,8 @@ namespace SourceBrowser.Shared
 {
     public class Constants
     {
-		public const string REPO_LOCK_FILENAME = "repo.lock";
-		public const string REPO_STATUS_PROCESSING = "Processing";
-		public const string REPO_STATUS_READY = "Ready";
+        public const string REPO_LOCK_FILENAME = "repo.lock";
+        public const string REPO_STATUS_PROCESSING = "Processing";
+        public const string REPO_STATUS_READY = "Ready";
     }
 }
--- src/SourceBrowser.Shared/RepositoryExistsException.cs
+++ src/SourceBrowser.Shared/RepositoryExistsException.cs
@@ -3,24 +3,24 @@ using System.Runtime.Serialization;
 
 namespace SourceBrowser.Shared
 {
-	[Serializable]
-	public class RepositoryExistsException : Exception, ISerializable
-	{
-		public RepositoryExistsException() : base()
+    [Serializable]
+    public class RepositoryExistsException : Exception, ISerializable
+    {
+        public RepositoryExistsException() : base()
         {
-		}
+        }
 
-		public RepositoryExistsException(string message) : base(message)
+        public RepositoryExistsException(string message) : base(message)
         {
-		}
+        }
 
-		public RepositoryExistsException(string message, Exception innerException) : base(message, innerException)
+        public RepositoryExistsException(string message, Exception innerException) : base(message, innerException)
         {
-		}
+        }
 
-		// This constructor is needed for serialization.
-		protected RepositoryExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        // This constructor is needed for serialization.
+        protected RepositoryExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
-		}
-	}
+        }
+    }
 }
