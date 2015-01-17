0f2fc3d4fed6269822a61e7c9e7f191dfe11c49c
Amadeus Wieczorek <git@amadeusw.com>
2015-01-16 17:26:58 -0800

    Creates shared project with consts and exceptions

---
+++ src/SourceBrowser.Shared/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceBrowser.Shared
+{
+    public class Constants
+    {
+		public const string REPO_LOCK_FILENAME = "repo.lock";
+		public const string REPO_STATUS_PROCESSING = "Processing";
+		public const string REPO_STATUS_READY = "Ready";
+    }
+}
