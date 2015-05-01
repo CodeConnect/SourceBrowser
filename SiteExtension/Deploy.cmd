271b1a1e761cb19946dd6fef9697106ed3a35e87
Jef King <jefkin@microsoft.com>
2015-05-01 15:49:07 -0700

    less

--- SiteExtension/Deploy.cmd
+++ SiteExtension/Deploy.cmd
@@ -2,8 +2,6 @@ SET FileToDelete=sourcebrowser.zip
 
 IF EXIST %FileToDelete% del /F %FileToDelete%
 
-IF EXIST %FileToDelete% exit 1
-
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('PublishProfiles', 'sourcebrowser.zip'); }"
 
 @if "%_echo%" neq "1" @echo off
