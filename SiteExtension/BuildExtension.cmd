aae516d704358dd2607017c1924e0c79da0e59e9
Jef King <jefkin@microsoft.com>
2015-05-01 14:46:55 -0700

    more better

--- SiteExtension/BuildExtension.cmd
+++ SiteExtension/BuildExtension.cmd
@@ -1,1 +1,4 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\src\SourceBrowser.Site\SourceBrowser.Site.csproj /p:DeployOnBuild=true /p:PublishProfile=local
+@echo %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\src\SourceBrowser.Site\SourceBrowser.Site.csproj /p:DeployOnBuild=true /p:PublishProfile=local
+
+
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('PublishProfiles', 'sourcebrowser.zip'); }"
